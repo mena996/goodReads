8d715628ebf4efb51712ee7b22309dca8dda2c09
mena996 <menaemad996@gmail.com>
2020-05-25 04:42:14 +0200

    fix top book,categure and author

--- routes/books.js
+++ routes/books.js
@@ -30,142 +30,191 @@ router.get('/', async (req, res, next) => {
     }
 });
 
-router.get('/:id', async(req, res, next)=>{
-    try {
-        const book = await BookModel.findById(req.params.id).populate('author').populate('category')
-            if (!book) next("Book Not found..");
-            else res.send(book);
-      } catch (err) {
-        next("Error fetching book..");
-      }
-});
 
 // router.get('/:id', (req, res, next) => {
-//     return BookModel.findById(req.params.id).populate('author').populate('category').exec((err, book) => {
-//         if (err) next(err);
-//         res.json(book);
-//     });
-// });
-
-router.post('/', auth.shouldBe('admin'), multer.upload.single('image'), async(req, res, next) => {
-    const url = req.protocol + '://' + req.get('host');
-    try {
-        const { name, category, author } = req.body;
-        const book = await BookModel.create({
-            name,
-            image: url + '/public/' + req.file.filename,
-            category,
-            author
-        });
-        res.send(book)
-    } catch{
-        next("Erorr while adding a book");
-    }
-});
-
-router.patch('/:id', auth.shouldBe('admin'), multer.upload.single('image'), async (req, res, next) => {
-    const url = req.protocol + '://' + req.get('host');
-    if(req.file) req.body.image = url + '/public/' + req.file.filename;
-    try {
-        const book = await BookModel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
-        if (!book) next("book not found");
-        else res.json(book);
-    } catch{
-        next("Error in editing book")
-    }
-});
-
-router.delete('/:id', auth.shouldBe('admin'), async (req, res, next) => {
-    try {
-        const book = await BookModel.findByIdAndDelete(req.params.id);
-        if (!book) next("book not found");
-        else res.json(book);
-    } catch{
-        next("Error in removing book")
-    }
-});
-
-router.post('/rate', async (req, res, next) => {
-    try {
-        const { rate, user, book } = req.body;
-        options = { upsert: true, new: true, setDefaultsOnInsert: true };
-        bookRate = await RateBookModel.findOneAndUpdate({ user, book }, { rate, user, book }, options);
-        res.send(bookRate)
-    } catch (err) {
-        next(err);
-    }
-});
-
-router.post('/shelf', async (req, res, next) => {
-    try {
-        const { state, user, book } = req.body;
-        options = { upsert: true, new: true, setDefaultsOnInsert: true };
-        bookState = await ShelvBookModel.findOneAndUpdate({ user, book }, { state, user, book }, options);
-        res.send(bookState)
-    } catch (err) {
-        next(err);
-    }
-});
-
-router.get('/rate', async (req, res, next) => {
-    try {
-        const { user, book } = req.body;
-        bookState = await RateBookModel.find({ user, book });
-        res.send(bookState)
-    } catch (err) {
-        next(err);
-    }
-});
-
-router.get('/shelf', async (req, res, next) => {
-    try {
-        const { user, book } = req.body;
-        bookState = await ShelvBookModel.find({ user, book });
-        res.send(bookState)
-    } catch (err) {
-        next(err);
-    }
-});
-
-router.get('/shelf/:id', async (req, res) => {
-    try {
-        books = await ShelvBookModel.find({}).populate({
-            path:'book',
-            populate:{
-                path:'author'
+    //     return BookModel.findById(req.params.id).populate('author').populate('category').exec((err, book) => {
+        //         if (err) next(err);
+        //         res.json(book);
+        //     });
+        // });
+        
+        router.post('/', auth.shouldBe('admin'), multer.upload.single('image'), async (req, res, next) => {
+            const url = req.protocol + '://' + req.get('host');
+            try {
+                const { name, category, author } = req.body;
+                const book = await BookModel.create({
+                    name,
+                    image: url + '/public/' + req.file.filename,
+                    category,
+                    author
+                });
+                res.send(book)
+            } catch{
+                next("Erorr while adding a book");
             }
         });
-        res.send(books);
-    } catch (err) {
-        next(err);
-    }
-});
-
-router.get('/topbooks', async (req, res, next) => {
-    try {
-        const { user, book } = req.body;
-        // bookState=await RateBookModel.find({}).populate('book').sort({rate: -1}).limit(5)
-
-        bookState = await RateBookModel.aggregate(
-            [
-                {
-                    $group:
-                    {
-                        _id: { "book": "$book", },
-                        rate: { $avg:"$rate" } ,
+        
+        router.patch('/:id', auth.shouldBe('admin'), multer.upload.single('image'), async (req, res, next) => {
+            const url = req.protocol + '://' + req.get('host');
+            if (req.file) req.body.image = url + '/public/' + req.file.filename;
+            try {
+                const book = await BookModel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+                if (!book) next("book not found");
+                else res.json(book);
+            } catch{
+                next("Error in editing book")
+            }
+        });
+        
+        router.delete('/:id', auth.shouldBe('admin'), async (req, res, next) => {
+            try {
+                const book = await BookModel.findByIdAndDelete(req.params.id);
+                if (!book) next("book not found");
+                else res.json(book);
+            } catch{
+                next("Error in removing book")
+            }
+        });
+        
+        router.post('/rate', async (req, res, next) => {
+            try {
+                const { rate, user, book } = req.body;
+                options = { upsert: true, new: true, setDefaultsOnInsert: true };
+                bookRate = await RateBookModel.findOneAndUpdate({ user, book }, { rate, user, book }, options);
+                res.send(bookRate)
+            } catch (err) {
+                next(err);
+            }
+        });
+        
+        router.post('/shelf', async (req, res, next) => {
+            try {
+                const { state, user, book } = req.body;
+                options = { upsert: true, new: true, setDefaultsOnInsert: true };
+                bookState = await ShelvBookModel.findOneAndUpdate({ user, book }, { state, user, book }, options);
+                res.send(bookState)
+            } catch (err) {
+                next(err);
+            }
+        });
+        
+        router.get('/rate', async (req, res, next) => {
+            try {
+                const { user, book } = req.body;
+                bookState = await RateBookModel.find({ user, book });
+                res.send(bookState)
+            } catch (err) {
+                next(err);
+            }
+        });
+        
+        router.get('/shelf', async (req, res, next) => {
+            try {
+                const { user, book } = req.body;
+                bookState = await ShelvBookModel.find({ user, book });
+                res.send(bookState)
+            } catch (err) {
+                next(err);
+            }
+        });
+        
+        router.get('/shelf/:id', async (req, res) => {
+            try {
+                books = await ShelvBookModel.find({}).populate({
+                    path: 'book',
+                    populate: {
+                        path: 'author'
                     }
+                });
+                res.send(books);
+            } catch (err) {
+                next(err);
+            }
+        });
+        
+        router.get('/topbooks', async (req, res, next) => {
+            try {
+                const bookState = await RateBookModel.aggregate(
+                    [
+                        {
+                            $group:
+                            {
+                                _id: "$book",
+                                rate: { $avg: "$rate" },
+                            }
+                        }
+                    ]
+                    ).sort({rate: -1}).limit(5);
+                    const bestBooks=bookState.map(bookState => bookState['_id']);
+                    let books=await BookModel.find({"_id": { "$in" : bestBooks}});
+                    res.send(books);
+                } catch (err) {
+                    console.log(err);
+                    next(err);
                 }
-            ]
-        )
+            });
 
-        res.send(bookState)
-    } catch (err) {
-        next(err);
-    }
-});
+            router.get('/topcats', async (req, res, next) => {
+                try {
+                    const bookState = await RateBookModel.aggregate(
+                        [
+                            {
+                                $group:
+                                {
+                                    _id: "$book",
+                                    rate: { $avg: "$rate" },
+                                }
+                            }
+                        ]
+                        ).sort({rate: -1}).limit(5);
+                        const bestBooks=bookState.map(bookState => bookState['_id']);
+                        let books=await BookModel.find({"_id": { "$in" : bestBooks}}).populate('category');
+                        res.send(books);
+                    } catch (err) {
+                        console.log(err);
+                        next(err);
+                    }
+                });
 
-router.use((err, req, res, next) => {
-    res.send("oh no there is some thing wrong happend :( \n" + err);
-});
+                router.get('/topauths', async (req, res, next) => {
+                    try {
+                        const bookState = await RateBookModel.aggregate(
+                            [
+                                {
+                                    $group:
+                                    {
+                                        _id: "$book",
+                                        rate: { $avg: "$rate" },
+                                    }
+                                }
+                            ]
+                            ).sort({rate: -1}).limit(5);
+                            const bestBooks=bookState.map(bookState => bookState['_id']);
+                            let books=await BookModel.find({"_id": { "$in" : bestBooks}}).populate('author');
+                            res.send(books);
+                        } catch (err) {
+                            console.log(err);
+                            next(err);
+                        }
+                    });
 
+
+
+
+
+
+            router.get('/:id', async (req, res, next) => {
+                try {
+                    const book = await BookModel.findById(req.params.id).populate('author').populate('category')
+                    if (!book) next("Book Not found..");
+                    else res.send(book);
+                } catch (err) {
+                    next("Error fetching book..");
+                }
+            });
+            
+            router.use((err, req, res, next) => {
+                res.send("oh no there is some thing wrong happend :( \n" + err);
+            });
+            
 module.exports = router;
